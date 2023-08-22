bc6040930b2f5a18eb5245d2302733aac41a8470
@fabiomath2 <83668076+fabiomath2@users.noreply.github.com>
2023-08-22 14:46:57 -0300

    Add files via upload
    update commit

---
+++ back/CalculadoraSimples.java
@@ -0,0 +1,13 @@
+package AW.CalculadoraSimples;
+
+public class CalculadoraSimples {
+
+    public static void main(String[] args) {
+
+        Metodos metodos = new Metodos();
+
+        System.out.println("\n -- Calculadora simples -- \n");
+        metodos.calcular();
+        System.out.println("\n -- Fim -- ");
+    }
+}
